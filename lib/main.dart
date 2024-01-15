503f347700a7867ad3965c17100706e368d0ad8a
Dhruvil Shah <dhruvilshah732@gmail.com>
2024-01-15 08:02:23 +0530

    project start

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(reel_downloader());
+}
+
+
+class reel_downloader extends StatelessWidget {
+  const reel_downloader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Container(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
